05a212bfcaeaad2a5befc098c88bcd6e531a8722
ldvy <hardway.studio@gmail.com>
2021-09-30 01:49:40 +0300

    Added model description

--- lab1/main.py
+++ lab1/main.py
@@ -61,6 +61,8 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+model.summary()
+
 model.fit(data_train, label_train, epochs=20, batch_size=1, verbose=1)
 
 
